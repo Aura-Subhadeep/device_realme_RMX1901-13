522ddf7438fbd8501b5d4d97237463eed702edc7
Rajat Gupta <rjtgupta84@gmail.com>
2021-04-12 14:58:30 +0530

    RMX1901: motor: Use Camera Helper JAVA APIs instead of motor hidl

--- device.mk
+++ device.mk
@@ -391,10 +391,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
-# Camera Motor
-PRODUCT_PACKAGES += \
-    vendor.lineage.camera.motor@1.0-service.realme_sdm710
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.realme_sdm710 \
