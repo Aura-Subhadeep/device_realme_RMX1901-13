0e71d106e89ce7c7345a565711bdfcdfcb6e5f1d
daniml3 <danimoral1001@gmail.com>
2021-10-18 14:15:18 +0000

    RMX1901: lights: aidl impl from xiaomi_sweet
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -231,7 +231,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.realme_sdm710
+    android.hardware.lights-service.realme_sdm710
 
 # Media
 PRODUCT_PACKAGES += \
