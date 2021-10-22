d33ded3672162ca5ae0b01a55295ad0951f0d30a
Kyle Harrison <khwebmail@gmail.com>
2021-10-22 12:38:00 +0200

    RMX1901: Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.
    
    Change-Id: Id7a49467eafa395cb044c62371c451bb38e14d08
    Signed-off-by: Sai Kishan <kishansai95@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -184,3 +184,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.early.app.duration=16500000 \
     debug.sf.earlyGl.sf.duration=13500000 \
     debug.sf.earlyGl.app.duration=21000000
+
+# Recovery
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
