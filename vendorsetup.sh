880ab051ffacdc1a725eb11df0a8952c229b9766
Parixshit <parikshit.tunlr@slmail.me>
2022-03-05 22:43:07 +0530

    RMX1901: Build Realme Dirac

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,7 @@ git clone git@github.com:ashim-anwar/kernel_realme_RMX1901-12.git -b 12.1 kernel
 # Realme In-Screen Proximity Patch
 echo 'Patching Realme In-Screen Proximity [3/3]'
 cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git && git cherry-pick 5e8f94a2db8c0a026f72b99c988b0cfdd64716e2 && cd ../..
+
+# Realme Dirac
+echo 'Adding Realme Dirac Support [6/6]'
+git clone https://github.com/techyminati/android_packages_apps_RealmeDirac.git -b lineage-19.1 packages/apps/RealmeDirac
