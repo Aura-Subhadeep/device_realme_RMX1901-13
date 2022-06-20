6df12ad23e1d420ac602774e08c47c3bfe531404
root <ashim.anwar22@gmail.com>
2022-06-20 08:39:28 +0000

    RMX1901: Introduced vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+echo 'Cloning Vendor tree [1/2]'
+# Vendor Tree
+git clone git@github.com:ashim-anwar/vendor_realme_RMX1901-12.git -b 12.1 vendor/realme/RMX1901
+
+echo 'Cloning Kernel tree [2/2]'
+# Kernel Tree
+git clone git@github.com:ashim-anwar/kernel_realme_RMX1901-12.git -b 12.1 kernel/realme/RMX1901
+
