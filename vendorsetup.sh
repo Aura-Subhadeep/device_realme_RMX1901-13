0f7be0f1056311e40a6ccc7b3257991bc6b7dac9
ashim-anwar <ashim.anwar22@gmail.com>
2022-08-07 15:21:02 +0000

    RMX1901:vendorsetup.sh: Included Realme In-Screen Proximity

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,11 @@
-echo 'Cloning Vendor tree [1/2]'
 # Vendor Tree
+echo 'Cloning Vendor tree [1/3]'
 git clone git@github.com:ashim-anwar/vendor_realme_RMX1901-12.git -b 12.1 vendor/realme/RMX1901
 
-echo 'Cloning Kernel tree [2/2]'
 # Kernel Tree
+echo 'Cloning Kernel tree [2/3]'
 git clone git@github.com:ashim-anwar/kernel_realme_RMX1901-12.git -b 12.1 kernel/realme/RMX1901
 
+# Realme In-Screen Proximity Patch
+echo 'Patching Realme In-Screen Proximity [3/3]'
+cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git && git cherry-pick 5e8f94a2db8c0a026f72b99c988b0cfdd64716e2 && cd ../..
