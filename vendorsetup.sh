3776e433795230952816612d0b661fe0593b16d9
ashim-anwar <ashim.anwar22@gmail.com>
2022-09-11 12:12:36 +0000

    RMX1901: Re-Factored vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,14 @@
 # Vendor Tree
-echo 'Cloning Vendor tree [1/3]'
-git clone git@github.com:ashim-anwar/vendor_realme_RMX1901-12.git -b 12.1 vendor/realme/RMX1901
+echo 'Cloning Vendor tree [1/4]'
+git clone git@github.com:ashim-anwar/vendor_realme_RMX1901.git -b 13 vendor/realme/RMX1901
 
 # Kernel Tree
-echo 'Cloning Kernel tree [2/3]'
-git clone git@github.com:ashim-anwar/kernel_realme_RMX1901-12.git -b 12.1 kernel/realme/RMX1901
+echo 'Cloning Kernel tree [2/4]'
+git clone git@github.com:ashim-anwar/kernel_realme_RMX1901.git -b x.319 kernel/realme/RMX1901
 
 # Realme In-Screen Proximity Patch
-echo 'Patching Realme In-Screen Proximity [3/3]'
-cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git && git cherry-pick 5e8f94a2db8c0a026f72b99c988b0cfdd64716e2 && cd ../..
+echo 'Patching Realme In-Screen Proximity [3/4]'
+cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git 13 && git cherry-pick f899827fc9d7eb03912362baba1945e5a377fe0e && cd ../..
 
 # Use Proton Clang For Compilation
 echo ' Using Proton Clang For Compilation [4/4]'
