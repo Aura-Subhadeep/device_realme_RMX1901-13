84ebab65f8f66dce979f4b681bbc4adff052af89
ashim-anwar <ashim.anwar22@gmail.com>
2022-11-19 09:44:39 +0000

    RMX1901: Re-Factored vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,15 @@
 # Vendor Tree
 echo 'Cloning Vendor tree [1/4]'
-git clone git@github.com:ashim-anwar/vendor_realme_RMX1901.git -b 13 vendor/realme/RMX1901
+git clone git@github.com:RMX1901/vendor_realme_RMX1901.git -b 13 vendor/realme/RMX1901
 
 # Kernel Tree
 echo 'Cloning Kernel tree [2/4]'
-git clone git@github.com:ashim-anwar/kernel_realme_RMX1901.git -b x.319 kernel/realme/RMX1901
+git clone git@github.com:RMX1901/kernel_realme_RMX1901.git -b x.319 kernel/realme/RMX1901
 
 # Realme In-Screen Proximity Patch
 echo 'Patching Realme In-Screen Proximity [3/4]'
-cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git 13 && git cherry-pick 516173c637ec14b264eb43c0970284c00f2fc4b1 && cd ../..
+cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git 13 && git cherry-pick 415118ebea7a0cc64801f1cf36ca2385674cdb3b && cd ../..
 
 # Use Proton Clang For Compilation
 echo ' Using Proton Clang For Compilation [4/4]'
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
