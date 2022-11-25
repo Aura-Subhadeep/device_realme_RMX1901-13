0c5731b4d332ec1f69a2c71b20c6dce54808fa7b
ashim-anwar <ashim.anwar22@gmail.com>
2022-11-25 13:12:50 +0000

    RMX1901: Nuked GcamGo
    Sources Already Building Aperture Cam

--- device.mk
+++ device.mk
@@ -105,7 +105,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libstdc++.vendor \
-    GcamGo \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libgui_vendor
 
