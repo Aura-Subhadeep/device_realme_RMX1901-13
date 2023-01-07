0017c384d0fa367ec284ef0396e16eafdfe0f6ba
Edwin Moquete <edwinmmoquete@gmail.com>
2023-01-07 09:28:14 +0000

    RMX1901: This prop has no effect as of T QPR1. This prop has no effect as of T QPR1.
    See: LineageOS/android_packages_modules_Bluetooth@cf25e33

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -79,7 +79,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.a2dp_offload.disabled=true \
     persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
     persist.vendor.bt.enable.splita2dp=true \
     persist.vendor.qcom.bluetooth.enable.splita2dp=true \
