c6bc0be4ab7e2ffd509ea3369785260f4b64539c
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-04-02 21:42:46 +0530

    RMX1901: Update remove packages list

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SafetyHubPrebuilt OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos Maps SafetyHubPrebuilt MusicFX GoogleTTS SoundAmplifierPrebuilt GoogleCamera PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AndroidAutoStubPrebuilt Calculator Recorder
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
