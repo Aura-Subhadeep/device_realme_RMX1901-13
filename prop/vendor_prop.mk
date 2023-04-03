f5b72da28edd61a7152fb5fd7ba739de94814758
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-04-03 11:05:22 +0530

    RMX1901:  Surfaceflinger supports background to 0

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -89,7 +89,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Blur
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.supports_background_blur=0 \
 
 # Cached app compaction
 persist.device_config.activity_manager.use_compaction=true
