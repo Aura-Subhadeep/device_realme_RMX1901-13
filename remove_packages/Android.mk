49848f5a46bd236d368c38c5db19eb6c4c939163
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-04-03 11:12:08 +0530

    RMX1901: Update remove packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos Maps SafetyHubPrebuilt MusicFX GoogleTTS SoundAmplifierPrebuilt GoogleCamera PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos Maps Safety MusicFX GoogleTTS SoundAmplifierPrebuilt GoogleCamera PixelLiveWallpaperPrebuilt
 LOCAL_OVERRIDES_PACKAGES := AndroidAutoStubPrebuilt Calculator Recorder
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
