cf69b97a53a52cd1154cad8f51f843012dee2cba
Aura-Subhadeep <subhadeepadhikari5@gmail.com>
2023-05-10 13:02:51 +0000

    RMX1901: Import more surface flinger flags  - For better smoothness and to eliminate lags  - QS animation, app opening & app switching seem to have improved.
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -159,6 +159,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true
     debug.sf.auto_latch_unsignaled=true
     debug.sf.enable_gl_backpressure=1
+    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
 
 # Phantom process monitoring
 sys.fflag.override.settings_enable_monitor_phantom_procs=false
